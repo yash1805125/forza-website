e9f3047264d6aecc4bdc7a033f0ab68b93e23dbc
Yash Samal <samaljyotiraditya@gmail.com>
2021-08-29 20:55:34 +0530

    My

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import { Cars } from "./Cars";
+import ReactPlayer from "react-player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    type: "Super Cars",
+    info: [],
+  };
+
+  // componentDidMount() {
+  //   this.setState({ info: Cars });
+  //   console.log("ComponentDidMount");
+  // }
+  handleChange = (event) => {
+    this.setState({ type: event.target.value });
+  };
+  handleClick = () => {
+    this.setState({ info: Cars });
+  };
+  handleb2 = () => {
+    this.setState({ info: [] });
+  };
+  render() {
+    return (
+      <div className="App">
+        <select class="s1" value={this.state.type} onChange={this.handleChange}>
+          <option>Super Cars</option>
+          <option>Super Fast Cars</option>
+          <option>Real Fast</option>
+          <option>Fast</option>
+          <option>Slow</option>
+        </select>
+
+        <button onClick={this.handleClick} class="b1">
+          Visualize Cars
+        </button>
+        <button class="b2" onClick={this.handleb2}>
+          Hide Cars
+        </button>
+
+        <div>
+          <div>
+            <h4>Intro Videos</h4>
+          </div>
+
+          <ReactPlayer
+            className="v1"
+            url="https://www.youtube.com/watch?v=9kVGdXEvTJ8"
+            controls
+            volume="0.5"
+            height="250px"
+            width="450px"
+          />
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=5xy4n73WOMM"
+            controls
+            height="250px"
+            width="450px"
+          />
+          <div>
+            <h5>Gameplay Videos</h5>
+          </div>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=KuV5aeSUxPo"
+            controls
+            height="250px"
+            width="450px"
+          />
+          <ReactPlayer
+            className="v2"
+            url="https://www.youtube.com/watch?v=kfI31yDiaPY"
+            controls
+            height="250px"
+            width="450px"
+          />
+        </div>
+        <br />
+        {this.state.info.map((value, index) => {
+          return (
+            <div>
+              <img
+                alt="Cars"
+                src={this.state.info[index].image}
+                width="700"
+                height="400"
+              />
+              <h2>Name: {this.state.info[index].name}</h2>
+              <p>Rating: {this.state.info[index].rating}</p>
+              <h3>Price: {this.state.info[index].price}</h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Cars.js
@@ -0,0 +1,78 @@
+export const Cars = [
+  {
+    id: 1,
+    name: "Koenigsegg Agera RS",
+    rating: 10,
+    image: "https://i.redd.it/58cm4pan7h721.jpg",
+    price: "1600000$",
+  },
+  {
+    id: 2,
+    name: "Venom GT",
+    rating: 10,
+    image: "https://i.redd.it/7w8ijqm96to41.png",
+    price: "1000000$",
+  },
+  {
+    id: 3,
+    name: "Bugatti Chiron",
+    rating: 10,
+    image:
+      "https://www.teamvvv.com/wp-content/uploads/2018/02/forza-motorsport-7-dell-car-pack-bugatti-chiron-screenshot.jpg",
+    price: "3800000$",
+  },
+  {
+    id: 4,
+    name: "Koenigsegg One:1",
+    rating: 10,
+    image:
+      "https://cdn.motor1.com/images/mgl/zvYk8/s3/koenigsegg-agera-one-of-1-for-sale.webp",
+    price: "1650000$",
+  },
+  {
+    id: 5,
+    name: "Bugatti Veyron",
+    rating: 9.9,
+    image:
+      "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8bf7510b-7162-421a-9fce-2490abcd74e5/ddmyl55-0d15d831-e003-4cfc-add8-b618a39cc51a.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhiZjc1MTBiLTcxNjItNDIxYS05ZmNlLTI0OTBhYmNkNzRlNVwvZGRteWw1NS0wZDE1ZDgzMS1lMDAzLTRjZmMtYWRkOC1iNjE4YTM5Y2M1MWEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.64uGD8--YZLSzEvjvBvpYnR4RNXp5C0D8nH_5EkLJXY",
+    price: "1400000$",
+  },
+  {
+    id: 6,
+    name: "Koenigsegg Agera",
+    rating: 9.6,
+    image:
+      "https://preview.redd.it/uyulcfrjn6711.jpg?auto=webp&s=942d1d39dbaf663a1850418d2772dea2f396bf15",
+    price: "3000000$",
+  },
+  {
+    id: 7,
+    name: "LaFerrari",
+    rating: 9.5,
+    image: "https://wallpaperaccess.com/full/195606.jpg",
+    price: "1400000$",
+  },
+  {
+    id: 8,
+    name: "McLaren P1",
+    rating: 9.3,
+    image: "https://i.redd.it/xvlpch7fou521.jpg",
+    price: "1350000$",
+  },
+  {
+    id: 9,
+    name: "Ford “Hoonigan” Mustang",
+    rating: 9.3,
+    image:
+      "https://i0.web.de/image/548/32657548,pd=1,f=responsive169-w950/ken-block-pikes-peak.jpg",
+    price: "100000$",
+  },
+  {
+    id: 10,
+    name: "Koenigsegg Regera",
+    rating: 9.2,
+    image:
+      "https://c4.wallpaperflare.com/wallpaper/808/920/955/koenigsegg-microsoft-game-2018-regera-hd-wallpaper-preview.jpg",
+    price: "1900000$",
+  },
+];
